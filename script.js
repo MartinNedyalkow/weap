e006ae1afe65cb58f8e7008265dfd48b925aa300
Martin Nedyalkov <brpraziel@gmail.com>
2019-03-11 16:11:42 +0200

    sdfd

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let city='Sofia'
 
-
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&appid=1e6d7c72e6293ab2397112f9121e9aec",
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=1e6d7c72e6293ab2397112f9121e9aec",
 function(data){
 console.log(data);
+$('.city').append(data.name)
+
 
 })
