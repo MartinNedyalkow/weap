d5638aa0e48c056d5b82ac627413763aa2dd4333
Martin Nedyalkov <brpraziel@gmail.com>
2019-03-12 13:22:52 +0200

    from telerik2

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ $('.cityName').append(cityN);
 $.getJSON("http://api.openweathermap.org/data/2.5/forecast?q="+ city + ",bg&appid=1e6d7c72e6293ab2397112f9121e9aec&units=metric",
 function(fiveday){
 console.log(fiveday);
-let icon1 = 'https://openweathermap.org/img/w/'+ fiveday.list[5].weather[0].icon + '.png';
+let icon1 = 'https://openweathermap.org/img/w/'+ fiveday.list[7].weather[0].icon + '.png';
 $('.icon1').attr('src',icon1);
-let temp1 = Math.floor(fiveday.list[5].main.temp);
+let temp1 = Math.floor(fiveday.list[7].main.temp);
 $('.temp1').append(temp1);
-let whetherName1 = fiveday.list[5].weather[0].main;
+let whetherName1 = fiveday.list[7].weather[0].main;
 let cityN1=fiveday.city.name;
 $('.style1').append(whetherName1);
 $('.cityName1').append(cityN1);
