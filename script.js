d3557c43235fcb10f6bba2f9339239386491ec20
Martin Nedyalkov <brpraziel@gmail.com>
2019-03-12 17:23:40 +0200

    SEarch

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+let a="London"
 
-let city='Sofia';
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid=1e6d7c72e6293ab2397112f9121e9aec",
+
+
+const funk =function(city){
+    city=$('#search').val()
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ city+"&units=metric&appid=1e6d7c72e6293ab2397112f9121e9aec",
 function(data){
 console.log(data);
 let icon = 'https://openweathermap.org/img/w/'+ data.weather[0].icon + '.png';
 $('.icon').attr('src',icon);
 let temp = Math.floor(data.main.temp);
-$('.temp').append(temp);
-let whetherName = data.weather[0].main;
+$('.temp').empty().append(temp);
+let whetherName = data.weather[0].description;
 let cityN=data.name;
-$('.style').append(whetherName);
-$('.cityName').append(cityN);
+$('.style').empty().append(whetherName);
+$('.cityName').empty().append(cityN);
 
 })
+$(".text").html(`The weather in ${city} is`)
+}
+
+
+
+$("#SearchButton").on("click",funk)
